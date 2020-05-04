f7daa6d321fd0ddaa54409efa13fd0e04b9622c9
Jasonnugraha <jason.nugraha97@gmail.com>
2020-05-05 01:15:37 +1000

    nishta menu screen

--- DiceRollerInJava.java
+++ DiceRollerInJava.java
@@ -1,37 +1,10 @@
 import java.util.Random;
 import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
  
 // dice roller java source code
 // Also outputs the dice face as ASCII art
-
-public  class StartMenu {
-    
-    Scanner in = new Scanner(System.in);{
-        System.out.println("-- Dice Roller Game --");
-        // Display the menu
-        System.out.println("1\t Start Game");
-        System.out.println("2\t Quit Game");
-        System.out.println("Please enter your choice:");
-         
-        getUserInput = userInput.nextInt();
-        if(getUserInput > 2 ){
-         System.out.println("Please make a valid entry");
-        getUserInput = userInput.nextInt();}
-    
-         do
-        {
-        switch (getUserInput)
-        {
-        case 1: main(); 
-        break; 
-        
-        case 2: scanner.close();
-        break;
-        }
-    }
-}
-
-
 
 public class DiceRollerInJava {
  
@@ -45,31 +18,38 @@ public class DiceRollerInJava {
                            { { 1, 0, 1 }, { 1, 0, 1 }, { 1, 0, 1 } } };
  
     public static void main(String[] args) {
-       
-        Scanner scanner = new Scanner(System.in);
 
+        Scanner scanner = new Scanner(System.in);
         DiceRollerInJava dice = new DiceRollerInJava();
-        while (true) {
-            int result = dice.roll();
-            System.out.println("dice face value:" + result);
-            dice.draw(result);
- 
-            System.out.println("Roll again? (type no to quit):");
-            String input = scanner.nextLine();
-            if (input.equalsIgnoreCase("n") || 
-                    input.equalsIgnoreCase("no")) {
-                System.out.println("Bye!");
-                scanner.close();
-                return;
+        StartMenu();
+        if (Checker()){
+            while (true) {
+                int result = dice.roll();
+                System.out.println("dice face value:" + result);
+                dice.draw(result);
+    
+                System.out.println("Roll again? (type no to quit):");
+                String input = scanner.nextLine();
+                if (input.equalsIgnoreCase("n") || input.equalsIgnoreCase("no")) {
+                    System.out.println("Bye!");
+                    scanner.close();
+                    return;
+                }
             }
         }
+        else {
+            System.out.println("Bye!");
+            scanner.close();
+            return;
+        }
+        
     }
- 
+
     // Draw the dice face using ascii characters
     private void draw(int value) {
         int[][] displayVal = faceConfig[value - 1];
         System.out.println("-----");
- 
+
         for (int i = 0; i < 3; i++) {
             System.out.print("|");
             for (int j = 0; j < 3; j++) {
@@ -82,12 +62,41 @@ public class DiceRollerInJava {
             System.out.println("|");
         }
         System.out.println("-----");
- 
+
     }
- 
+
     // Roll the dice in Java
     private int roll() {
         Random r = new Random();
         return r.nextInt(6) + 1;
     }
+
+    private static void StartMenu() {
+    
+        System.out.println("-- Dice Roller Game --");
+        // Display the menu
+        System.out.println("1\t Start Game");
+        System.out.println("2\t Quit Game");
+        System.out.println("Please enter your choice:");
+            
+        
+    }
+    private static boolean Checker(){
+        Scanner in = new Scanner(System.in);
+        int getUserInput;
+        while(true){
+            getUserInput = in.nextInt();
+            if (getUserInput == 1 || getUserInput == 2){
+                break;
+            } else {
+            System.out.println("Please make a valid entry");
+            }
+        }
+        if(getUserInput == 1){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
